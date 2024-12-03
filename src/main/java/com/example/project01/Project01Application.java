e7bed0607c015ed3918e79c90a9e3d9ff5472f88
you <20211939@edu.hanbat.ac.kr>
2024-12-03 12:20:39 +0900

    hellotest

--- src/main/java/com/example/project01/Project01Application.java
+++ src/main/java/com/example/project01/Project01Application.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Project01Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Project01Application.class, args);
+        System.out.println("hello");
     }
-
 }
