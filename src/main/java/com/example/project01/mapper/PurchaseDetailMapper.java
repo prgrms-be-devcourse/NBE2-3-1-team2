24368ac6c3568f0b19dab357291ac7342a0eff84
you <20211939@edu.hanbat.ac.kr>
2024-12-12 00:20:33 +0900

    folder_change

--- src/main/java/com/example/project01/mapper/PurchaseDetailMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.project01.mapper;
-
-import com.example.project01.dto.PurchaseDetailTO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface PurchaseDetailMapper {
-    int purchaseDetailInfo(PurchaseDetailTO purchaseDetailTO);
-}
--- src/main/resources/static/js/cart.js
+++
@@ -1,6 +0,0 @@
-function cartIcon() {
-    let existingCart = JSON.parse(localStorage.getItem('cart')) || {}; // localStorage에서 값 가져오기
-    //장바구니 아이콘에 표시하기
-    let productCount = Object.keys(existingCart).length;
-    document.getElementById('cart-counter').innerHTML = productCount;
-}
